622a8c363367e10b7a884a69429e7a4a32a552a9
Justin Wayne <wayne.jch@gmail.com>
2016-02-15 14:21:26 -0600

    "configuring app for a heroku deployment - server.js complete"

---
+++ server.js
@@ -0,0 +1,48 @@
+// var browserify = require('browserify-middleware')
+var express = require('express')
+var Path = require('path')
+
+var routes = express.Router()
+
+//
+// Provide a browserified file at a specified path
+//
+// routes.get('/app-bundle.js',
+//   browserify('./client/app.js'))
+
+//makes assets publicly available
+var assetFolder = Path.resolve(__dirname, './public')
+routes.use(express.static(assetFolder))
+
+
+if (process.env.NODE_ENV !== 'test') {
+  //
+  // The Catch-all Route
+  // This is for supporting browser history pushstate.
+  // NOTE: Make sure this route is always LAST.
+  //
+  routes.get('/*', function(req, res){
+    res.sendFile( assetFolder + '/index.html' )
+  })
+
+  //
+  // We're in development or production mode;
+  // create and run a real server.
+  //
+  var app = express()
+
+  // Parse incoming request bodies as JSON
+  app.use( require('body-parser').json() )
+
+  // Mount our main router
+  app.use('/', routes)
+
+  // Start the server!
+  var port = process.env.PORT || 4000
+  app.listen(port)
+  console.log("Listening on port", port)
+}
+else {
+  // We're in test mode; make this file importable instead.
+  module.exports = routes
+}
